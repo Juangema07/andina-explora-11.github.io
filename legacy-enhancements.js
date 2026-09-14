299303bc55126f2fc5a465b9f729e0df7e2361d3
Juangema07 <153125488+Juangema07@users.noreply.github.com>
2026-09-13 19:08:25 -0500

    Load Viaje Andino quality fixes

--- legacy-enhancements.js
+++ legacy-enhancements.js
@@ -5,7 +5,8 @@ const files=[
   'andina-games-fix.js?v=20260913-2',
   'andina-image-fix.js?v=20260913-2',
   'conecta-fix.js?v=20260913-1',
-  'games-position-fix.js?v=20260913-1'
+  'games-position-fix.js?v=20260913-1',
+  'viaje-quality-fix.js?v=20260913-2'
 ];
 function load(src){
   return new Promise((resolve,reject)=>{
